648ec409d18085194e4545a6b561b961f410ef33
Yoselyn Bascunan <yoselyn.bascunan@inacapmail.cl>
2023-04-11 21:35:09 -0400

    Base de datos

--- ProyectoTapizados/settings.py
+++ ProyectoTapizados/settings.py
@@ -80,8 +80,10 @@ pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Tapizados',
+        'USER':'root',
+        'PASSWORD':'Wizard2023.'
     }
 }
 
