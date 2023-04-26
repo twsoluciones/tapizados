fcf9cef3e6b131ed60c4cf26052513d755d68c6a
Felipe <luis.munoz209@inacapmail.cl>
2023-04-26 19:49:49 -0400

    cambios html

--- ProyectoTapizados/settings.py
+++ ProyectoTapizados/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Tapizados',
         'USER':'root',
-        'PASSWORD':'Wizard2023.'
+        'PASSWORD':'Cloud@184889137'
     }
 }
 
